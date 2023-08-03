131bcb3d381b4942cef5be8749e2ed4d6721f907
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-08-03 20:23:08 -0300

    feat(add): splash screen

--- app/src/main/java/com/example/hamradio/MainActivity.kt
+++ app/src/main/java/com/example/hamradio/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.hamradio
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    supportActionBar?.hide()
+    window.statusBarColor = Color.parseColor("#51576d")
+
+    Handler(Looper.getMainLooper()).postDelayed({
+      val intent = Intent(this, Home::class.java)
+      startActivity(intent)
+      finish()
+    }, 3000)
+
   }
 }
